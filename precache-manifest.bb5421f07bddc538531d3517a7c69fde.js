7fcf45225b0eaa93a66516fc270f41296a123e39
DarbhaRamesh <darbha.ramesh1996@gmail.com>
2020-06-30 17:09:26 +0530

    Updates

---
+++ precache-manifest.bb5421f07bddc538531d3517a7c69fde.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30431b1cedf46d39b4d6d13bde5bd995",
+    "url": "/First-React-App/index.html"
+  },
+  {
+    "revision": "46d096e9df8d9019e2d3",
+    "url": "/First-React-App/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "7b3db2d9a9d96658b49c",
+    "url": "/First-React-App/static/css/main.24d351c1.chunk.css"
+  },
+  {
+    "revision": "46d096e9df8d9019e2d3",
+    "url": "/First-React-App/static/js/2.d4dd4716.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/First-React-App/static/js/2.d4dd4716.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b3db2d9a9d96658b49c",
+    "url": "/First-React-App/static/js/main.4d592988.chunk.js"
+  },
+  {
+    "revision": "04f8312cb97f45b6f301",
+    "url": "/First-React-App/static/js/runtime-main.6e87b979.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/First-React-App/static/media/SEGA.12a3db8b.woff"
+  }
+]);
